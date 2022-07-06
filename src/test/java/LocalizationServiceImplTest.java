b0255f8aa9297ebe8eeeb3d6332ffbde218d0064
SVKotov <syndareon@gmail.com>
2022-07-06 20:57:54 +0300

    Edited

--- src/test/java/LocalizationServiceImplTest.java
+++ src/test/java/LocalizationServiceImplTest.java
@@ -1,45 +1,25 @@
-import org.junit.jupiter.api.*;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.mockito.Mockito;
-import ru.netology.i18n.LocalizationService;
 import ru.netology.i18n.LocalizationServiceImpl;
+import ru.netology.i18n.LocalizationService;
 
-import static ru.netology.entity.Country.RUSSIA;
-import static ru.netology.entity.Country.USA;
+import static ru.netology.entity.Country.*;
+
 
 public class LocalizationServiceImplTest {
 
-    static LocalizationService localizationService;
-
-    @BeforeAll
-    public static void initSuite() {
-
-        localizationService = Mockito.mock(LocalizationServiceImpl.class);
-
-        Mockito.when(localizationService.locale(RUSSIA)).thenReturn("Добро пожаловать");
-        Mockito.when(localizationService.locale(USA)).thenReturn("Welcome");
-
-        System.out.println("Start tests for methods of class 'LocalizationServiceImpl' ....");
-    }
-
-    @AfterAll
-    public static void completeSuite() {
-        System.out.println("All tests for methods of class 'LocalizationServiceImpl' complete!");
-    }
-
     @Test
-    @DisplayName("Test locale() for RUSSIA")
-    public void localeRussiaTest(TestInfo localeTestInfo) {
-        Assertions.assertEquals(localizationService.locale(RUSSIA), "Добро пожаловать",
-                localeTestInfo.getDisplayName() + " is NO complete !");
-        System.out.println(localeTestInfo.getDisplayName() + " complete !");
+    public void contentOfTheMessageByCountryTest () {
+
+        LocalizationService localizationService = new LocalizationServiceImpl();
+
+        Assertions.assertEquals(localizationService.locale(RUSSIA), "Добро пожаловать");
+        Assertions.assertEquals(localizationService.locale(GERMANY), "Welcome");
+        Assertions.assertEquals(localizationService.locale(USA), "Welcome");
+        Assertions.assertEquals(localizationService.locale(BRAZIL), "Welcome");
+
     }
 
-    @Test
-    @DisplayName("Test locale() for USA")
-    public void localeUsaTest(TestInfo localeTestInfo) {
-        Assertions.assertEquals(localizationService.locale(USA), "Welcome",
-                localeTestInfo.getDisplayName() + " is NO complete !");
-        System.out.println(localeTestInfo.getDisplayName() + " complete !");
-    }
 }
 
